320babd57233e1bfb80b550bba0a18272dc6b92c
Amelie Trautwein <Amelie.Trautwein@tum.de>
2025-11-19 14:36:30 +0100

    split script

--- scripts/split_script.py
+++ scripts/split_script.py
@@ -35,7 +35,8 @@ def main():
 
     splits = stratified_split_by_entities(df,
                                           protein_col=protein_col,
-                                          substrate_col=substrate_col)
+                                          substrate_col=substrate_col,
+                                          plot=True)
 
     # check stratification
     c1 = splits['C1']
